8c6fb68370b830b5daaeb8e020a5d32936223a79
cpchary <prashanthcharys@gmail.com>
2016-07-24 22:45:34 -0500

    Job Portal

---
+++ JobSeeker/footer.php
@@ -0,0 +1,10 @@
+   <!-- Footer -->
+    <div id="footer">
+        <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
+        <hr class="noscreen" />
+        
+        <p  id="createdby">created by Varun
+          <!-- DON´T REMOVE, PLEASE! -->
+        </p>
+        <p id="copyright">&copy; 2014jobportal.com</p>
+  </div> <!-- /footer -->
